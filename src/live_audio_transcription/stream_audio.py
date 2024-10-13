9532ff9c16a3f7d5a93c447c8817a33dfbc3f5b2
Izaan Khalid <izaan.khalid@outlook.com>
2024-10-13 02:46:44 -0500

    Stream Audio Adds to VectorDB

--- src/live_audio_transcription/stream_audio.py
+++ src/live_audio_transcription/stream_audio.py
@@ -1,17 +1,14 @@
 import pyaudio 
 import wave 
-import requests
 import time
 import os 
-from moviepy.editor import AudioFileClip
 from groq import Groq
-from dotenv import load_dotenv
-import json
 import numpy as np
 import streamlit as st
 from podcast.speech_to_text import audio_to_text
 from datetime import datetime
-import threading
+from podcast.embedding import store_embeddings
+from langchain.docstore.document import Document
 
 # Audio stream parameters
 CHUNK = 8192
@@ -19,6 +16,9 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
 DURATION = 5
+
+# List to store the chunkz of text
+transcription_chunks = list()
 
 # Number of samples for 10 seconds
 NUM_SAMPLES = RATE * DURATION  # 160,000 samples for 10 seconds
@@ -37,6 +37,8 @@ model = 'whisper-large-v3'
 # Global variable to control recording
 if "recording" not in st.session_state:
     st.session_state.recording = False
+if "docsearch" not in st.session_state:
+    st.session_state.docsearch = None
 
 def save_chunk_wav(audio_chunk, chunk_number):
     filename = os.path.join(mp3_chunk_folder, f"chunk_{chunk_number}_{datetime.now().strftime('%Y%m%d%H%M%S')}.wav")
@@ -46,6 +48,11 @@ def save_chunk_wav(audio_chunk, chunk_number):
         wf.setframerate(RATE)
         wf.writeframes(audio_chunk.tobytes())
     return filename
+
+def split_text_into_chunks(text, word_limit=4096):
+    words = text.split()
+    for i in range(0, len(words), word_limit):
+        yield " ".join(words[i:i + word_limit])
 
 def start_recording():
     global recording 
@@ -59,6 +66,8 @@ def start_recording():
     st.write("Recording...")
     chunk_number = 0
 
+    st.write("Live Transcription: ")
+    full_text = ""
     while st.session_state.recording:
         data = stream.read(NUM_BYTES, exception_on_overflow=False)
         audio_chunk = np.frombuffer(data, dtype=np.int16)
@@ -67,13 +76,20 @@ def start_recording():
         chunk_number+=1
 
         transcription = audio_to_text(chunk_path)
-        st.write(f"Live Transcription: {transcription}")
+        full_text += transcription
+        if(len(full_text) > 4096):
+            text_chunks = split_text_into_chunks(full_text)
+            for i, chunk in enumerate(text_chunks):
+                transcription_chunks.append(chunk)
+            full_text = ""
+
+        st.write(transcription)
         time.sleep(0.01)
+        print(transcription_chunks)
     
     stream.stop_stream()
     stream.close()
     p.terminate()
-    st.write("Recording stopped.")
 
 def start_transcription():
     st.session_state.recording = True
@@ -82,3 +98,6 @@ def start_transcription():
 def stop_transcription():
     """Function to stop recording."""
     st.session_state.recording = False 
+    st.write("Recording stopped.")
+    documents = [Document(page_content=chunk) for chunk in transcription_chunks]
+    st.session_state.docsearch = store_embeddings(documents)
