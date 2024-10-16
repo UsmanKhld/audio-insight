bb53e9d3d96bf36fa13132471e1b1af769b09e66
Usman Khalid <usmankhld602@gmail.com>
2024-10-15 19:21:46 -0500

    small change

--- src/podcast/speech_to_text.py
+++ src/podcast/speech_to_text.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("GROQ_API_KEY")
-client = Groq(api_key = API_KEY)
+client = Groq(api_key=API_KEY)
 model = 'whisper-large-v3'
 
 def audio_to_text(filepath, translate_to_english):
